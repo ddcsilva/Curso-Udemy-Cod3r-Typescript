15f717262ed419112e338e3949caf8b1d1448641
Danilo Silva <danilo.silva@msn.com>
2023-02-02 06:20:45 -0300

    Tipos Numéricos E Booleano

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -2,3 +2,14 @@
 let nome = 'João'
 console.log(nome)
 // nome = 28
+
+// numbers
+let idade = 27
+// idade = 'Ana'
+idade = 27.5
+console.log(idade)
+
+// boolean
+let possuiHobbies = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
